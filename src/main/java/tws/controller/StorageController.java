c21a996205d88855bbb4b292c291eff2f99afb8a
wangqiming1001 <512821390@qq.com>
2019-09-05 18:00:50 +0800

    包裹入库

--- src/main/java/tws/controller/StorageController.java
+++ src/main/java/tws/controller/StorageController.java
@@ -5,18 +5,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import tws.entity.Employee;
 import tws.entity.Storage;
 import tws.service.StorageService;
 
 @RestController
 @RequestMapping("/storage")
+@CrossOrigin("*")
 public class StorageController {
 
 	@Autowired
